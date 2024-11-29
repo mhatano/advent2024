daf287a5fcc4df630aa050e87b2e66e14017a54b
Manami Hatano <hatano.manami@gmail.com>
2024-11-30 00:47:35 +0900

    Implement external provider list file handling

--- src/jp/hatano/advent2024/main/AdventApp.java
+++ src/jp/hatano/advent2024/main/AdventApp.java
@@ -1,27 +1,15 @@
 package jp.hatano.advent2024.main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 
 public class AdventApp {
-    private static final String[][] listProviders = {
-        { "Japanese", "jp.hatano.advent2024.providers.JapaneseMessageProvider" },
-        { "English",  "jp.hatano.advent2024.providers.EnglishMessageProvider" },
-    };
     public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            for ( int i = 0 ; i < listProviders.length ; i++ ) {
-                System.out.printf("%d: %s\n",i,listProviders[i][0]);
-            }
-            System.out.print("Your choice: ");
-            String input = reader.readLine();
-            int inputNumber = Integer.parseInt(input);
-            if ( inputNumber >= 0 && inputNumber < listProviders.length ) {
-                Class<?> aClass = Class.forName(listProviders[inputNumber][1]);
+            BufferedReader reader = new BufferedReader(new FileReader("list.txt"));
+            while ( reader.ready() ) {
+                String providerName = reader.readLine();
+                Class<?> aClass = Class.forName(providerName);
                 AdventApp application = new AdventApp((AdventMessageProvider)aClass.getConstructor(new Class[]{}).newInstance(new Object[] {}));
                 application.sayHello(System.out);
             }
