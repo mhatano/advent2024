badf0f8e2f243b16e4cce3b9689a84661ad636d6
Manami Hatano <hatano.manami@gmail.com>
2024-11-29 23:32:01 +0900

    Using Class Declared.

--- src/jp/hatano/advent2024/main/AdventApp.java
+++ src/jp/hatano/advent2024/main/AdventApp.java
@@ -1,7 +1,14 @@
 package jp.hatano.advent2024.main;
 
+import java.io.PrintStream;
+
 public class AdventApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        AdventApp application = new AdventApp();
+        application.sayHello(System.out);
+    }
+
+    public void sayHello(PrintStream out) {
+        out.println("Hello, World!");
     }
 }
