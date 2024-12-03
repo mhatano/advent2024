428e345b6c51a5cb9c235db1067c3de8f998e7bb
Manami Hatano <hatano.manami@gmail.com>
2024-12-03 23:07:32 +0900

    Another way to use Provider

--- src/jp/hatano/advent2024/main/AdventMessageProviderFactory.java
+++ src/jp/hatano/advent2024/main/AdventMessageProviderFactory.java
@@ -1,24 +1,38 @@
 package jp.hatano.advent2024.main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.File;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystem;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.nio.file.Paths;
 
 public class AdventMessageProviderFactory implements Iterable<AdventMessageProvider>, Iterator<AdventMessageProvider> {
     private final List<AdventMessageProvider> listProviders = new ArrayList<>();
-    public AdventMessageProviderFactory(String filename) {
+    public AdventMessageProviderFactory(String packageName) {
+        String currentClassPath = null;
+        String thisPackageName = getClass().getPackageName().replace(".",File.separator);
+        String targetPackageName = packageName.replace(".",File.separator);
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
-            while ( reader.ready() ) {
-                String providerName = reader.readLine();
-                Class<?> aClass = Class.forName(providerName);
-                listProviders.add((AdventMessageProvider)aClass.getConstructor(new Class[]{}).newInstance(new Object[] {}));
+            URL classPath = getClass().getResource(".");
+            if ( classPath != null ) {
+                currentClassPath = Paths.get(classPath.toURI()).toAbsolutePath().toString().replace(thisPackageName,targetPackageName);
+                File[] providerFiles = new File(currentClassPath).listFiles();
+                if ( providerFiles != null ) {
+                    for (File providerFile : providerFiles) {
+                        String providerAbsolutePath = providerFile.getAbsolutePath();
+                        String filename = providerAbsolutePath.substring(providerAbsolutePath.lastIndexOf(File.separatorChar)+1);
+                        String className = packageName + "." + filename.replace(".class","");
+                        Class<?> aClass = Class.forName(className);
+                        listProviders.add((AdventMessageProvider) aClass.getConstructor(new Class[]{}).newInstance(new Object[]{}));
+                    }
+                }
             }
-        } catch (IOException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+        } catch (URISyntaxException | NoSuchMethodException | IllegalAccessException | InstantiationException |
+                 InvocationTargetException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
